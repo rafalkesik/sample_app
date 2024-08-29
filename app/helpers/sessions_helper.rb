e20adfc58d1f0de64e6b7ad6fac4ab03a64ee685
Rafał Kęsik <kesik.rafal@yahoo.com>
2024-08-29 15:27:45 +0200

    Finish log in/log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,23 +3,51 @@ module SessionsHelper
     # Logs in the given user.
     def log_in(user)
         session[:user_id] = user.id
+        # Guard against session raplay attacks.
+        # See https://bit.ly/33UvK0w for more.
+        session[:session_token] = user.session_token
     end
-
+    
+    # Remembers the login state in permanent cookie.
+    def remember(user)
+        user.remember
+        cookies.permanent.encrypted[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+    end
+    
     # Returns the current logged-in user (if any).
     def current_user
-        if session[:user_id]
-            @current_user ||= User.find_by(id: session[:user_id])
+        if (user_id = session[:user_id])
+            user = User.find_by(id: user_id)
+            if user && session[:session_token] == user.session_token
+                @current_user = user
+            end
+        elsif (user_id = cookies.encrypted[:user_id])
+            user = User.find_by(id: user_id)
+            if user && user.authenticated?(cookies[:remember_token])
+                log_in user
+                @current_user = user
+            end
         end
     end
-
+    
     # Returns true if the user is logged in, false otherwise.
     def logged_in?
         !current_user.nil?
     end
 
+    # Forgets a persistent session
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
+    end
+
     # Logs out a user
     def log_out
-        session.delete(:user_id)
+        forget(current_user)
+        reset_session
         @current_user = nil
     end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -71,4 +71,9 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
   end
+
+  # test explanation: @user is not saved to db, so its remember_digest is nil.
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
